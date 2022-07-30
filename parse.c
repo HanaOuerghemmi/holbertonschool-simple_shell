87914cde1aa1004d9815e8d14115fd450948ec35
majdi aribi <majdi.developer@gmail.com>
2022-07-30 10:45:36 +0100

    fix some problem in readline && prompt

--- parse.c
+++ parse.c
@@ -8,10 +8,10 @@
 char **parse(char* input) 
 {
 	unsigned long i;
-	int all_tokens_buffer = 100;
+	int all_tokens_buffer = 2;
 	char **all_tokens = malloc(all_tokens_buffer * sizeof(char *));
 	int all_tokens_counter = 0;
-	int current_token_buffer = 1024;
+	int current_token_buffer = 2;
 	char *current_token = malloc(current_token_buffer * sizeof(char *));
 	int current_token_counter = 0;
 
@@ -25,13 +25,13 @@ char **parse(char* input)
 
 			if (all_tokens_counter + 1 > all_tokens_buffer) 
 			{
-				all_tokens_buffer += 100;
+				all_tokens_buffer += all_tokens_buffer;
 				all_tokens = realloc(all_tokens, all_tokens_buffer * sizeof(char *));
 			}
 
 			if (current_token_counter + 1 >= current_token_buffer) 
 			{
-				current_token_buffer += 1024;
+				current_token_buffer += current_token_buffer;
 				current_token = realloc(current_token, current_token_buffer);
 			}
 
@@ -48,13 +48,13 @@ char **parse(char* input)
 
 			if (all_tokens_counter + 2 > all_tokens_buffer) 
 			{
-				all_tokens_buffer += 100;
+				all_tokens_buffer += all_tokens_buffer;
 				all_tokens = realloc(all_tokens, all_tokens_buffer * sizeof(char *));
 			}
 
 			if (current_token_counter + 1 > current_token_buffer) 
 			{
-				current_token_buffer += 1024;
+				current_token_buffer += current_token_buffer;
 				current_token = realloc(current_token, current_token_buffer);
 			}
 
@@ -70,7 +70,7 @@ char **parse(char* input)
 
 			if (current_token_counter + 1 > current_token_buffer) 
 			{
-				current_token_buffer += 1024;
+				current_token_buffer += current_token_buffer;
 				current_token = realloc(current_token, current_token_buffer);
 			}
 
