75d24dabfe8be35cd14ae4cafa086d981cc62110
HanaOuerghemmi <5004@holbertonschool.com>
2022-08-02 19:38:30 +0100

    betty style

--- main.c
+++ main.c
@@ -1,21 +1,29 @@
 #include "shell.h"
-int main() {
-	
+/**
+ * main - program main of the shell
+ * Return: 0
+ */
+int main(void)
+{
+
 	char *line;
 	char **tokens;
 
-    while (true) {
-      /*  printf("$ ");*/
-        line = read_line();
-        tokens = split_line(line);
+	while (true)
+	{
+		/*  printf("$ ");*/
+		line = read_line();
+		tokens = split_line(line);
 
-        if (tokens[0] != NULL) {
-            exec(tokens);
-        }
 
-        free(tokens);
-        free(line);
-    }
+		if (tokens[0] != NULL)
+		{
+			exec(tokens);
+		}
+
+		free(tokens);
+		free(line);
+	}
 	return (0);
 }
 
--- read_line.c
+++ read_line.c
@@ -1,21 +1,24 @@
-
 #include "shell.h"
+/**
+ * read_line - function that read the commande line
+ * Return: char*
+ */
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t buflen = 0;
+	ssize_t len;
+	int err = 0;
 
-
-char *read_line() {
-    char *line = NULL;
-    size_t buflen = 0;
-    ssize_t strlen;
-	
-    int errno = 0;
-
-	strlen = getline(&line, &buflen, stdin);
-    if (strlen < 0) {
-        if (errno) {
-            perror("shell");
-        }
+	len = getline(&line, &buflen, stdin);
+	if (strlen < 0)
+	{
+		if (err)
+		{
+			perror("shell");
+		}
 		free(line);
-        exit(0);
-    }
-    return line;
+		exit(0);
+	}
+	return (line);
 }
